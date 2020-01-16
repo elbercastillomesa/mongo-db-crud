49b92155161a555fbbb901c3fbf289ee5ca79ba9
Elber <elbercastillomesa@gmail.com>
2020-01-16 12:29:23 -0500

    new test created to handle find ONE record in the collection

---
+++ test/finding_test.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const Provider = require('../models/providers');
+
+// Describe Tests
+describe('Finding Record', function(){
+
+  beforeEach(function(done){
+
+        const event = new Date();
+        const identity = new mongoose.Types.ObjectId();
+        var provider = new Provider(
+          {
+            _id : identity,
+            firstName : "Test_1",
+            lastName : "Test_1",
+            middleName : "None",
+            email : "test_1@hotmail.com",
+            specialty : {
+              _id : identity,
+              name : "Adolescent Medicine",
+              createdBy : 3838,
+              createdAt : event.toISOString(),
+              updatedBy : 82768,
+              updatedAt : event.toISOString()
+            },
+            projectedStartDate : event.toISOString(),
+            employerId : 318,
+            providerType : "MD",
+            staffStatus : "ACTIVE",
+            assignedTo : 66523,
+            status : "READY_FOR_REVIEW",
+            createdBy : 79030,
+            createdAt : event.toISOString(),
+            updatedBy : 16969,
+            updatedAt : event.toISOString()
+          }
+        );
+
+        provider.save().then( function(){
+          assert(provider.isNew === false);
+        }).then(done,done);
+  })
+  //Create Tests
+  it('Find One Record in Collection', function(done){
+
+    Provider.findOne({firstName:"Test_1"}).then(function(result){
+      assert(result.firstName === "Test_1");
+    }).then(done,done);
+  });
+});
