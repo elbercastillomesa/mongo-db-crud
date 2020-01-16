d703cd121a737c981175702b3202aca1598a1505
Elber <elbercastillomesa@gmail.com>
2020-01-16 11:02:57 -0500

    fixed warnings in mongoose connection

--- test/connection.js
+++ test/connection.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
-// Connection to Mongo DB
-mongoose.connect('mongodb://ecastillo:ecastillo1@ds135624.mlab.com:35624/evercheck-test-4');
+//ES6 Promise
+mongoose.Promise = global.Promise;
 
-mongoose.connection.once( 'open', function(){
-  console.log('Connection Successfull')
-}).on('error', function(error){
-  console.log('Connection Error: ',error);
-})
+// Fix DB connection before test run
+before(function(done){
+  // Connection to Mongo DB
+  mongoose.connect(
+    'mongodb://ecastillo:ecastillo1@ds135624.mlab.com:35624/evercheck-test-4',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+
+  mongoose.connection.once( 'open', function(){
+    console.log('Connection Successfull');
+    done();
+  }).on('error', function(error){
+    console.log('Connection Error: ',error);
+  })
+});
--- test/demo_test.js
+++ test/demo_test.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const mocha = require('mocha');
 const assert = require('assert');
 const Provider = require('../models/providers');
@@ -9,38 +10,39 @@ describe('Saving test', function(){
   it('Saves a provider in database', function(done){
 
     const event = new Date();
-
-    var provider = new Provider({
-      _id : ObjectId("59bafa00c52f6a85ec5ff993"),
-      firstName : Test_1,
-      lastName : Test_1,
-      middleName : None,
-      email : test_1@hotmail.com,
-      specialty : {
-        _id : ObjectId("59baf9b7c52f6a85ec5ff962"),
-        name : Adolescent Medicine,
-        createdBy : 3838,
+    const identity = new mongoose.Types.ObjectId();
+    var provider = new Provider(
+      {
+        _id : identity,
+        firstName : "Test_1",
+        lastName : "Test_1",
+        middleName : "None",
+        email : "test_1@hotmail.com",
+        specialty : {
+          _id : identity,
+          name : "Adolescent Medicine",
+          createdBy : 3838,
+          createdAt : event.toISOString(),
+          updatedBy : 82768,
+          updatedAt : event.toISOString()
+        },
+        projectedStartDate : event.toISOString(),
+        employerId : 318,
+        providerType : "MD",
+        staffStatus : "ACTIVE",
+        assignedTo : 66523,
+        status : "READY_FOR_REVIEW",
+        createdBy : 79030,
         createdAt : event.toISOString(),
-        updatedBy : 82768,
+        updatedBy : 16969,
         updatedAt : event.toISOString()
-      },
-      projectedStartDate : event.toISOString(),
-      employerId : 318,
-      providerType : MD,
-      staffStatus : ACTIVE,
-      assignedTo : 66523,
-      status : READY_FOR_REVIEW,
-      createdBy : 79030,
-      createdAt : event.toISOString(),
-      updatedBy : 16969,
-      updatedAt : event.toISOString()
       }
-    });
+    );
 
     provider.save().then( function(){
       assert(provider.isNew === false);
       done();
-    })
-
+    });
+    done();
   });
 });
