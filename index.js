532b1b7350e79f4d11e6264fce55f8819b7b5338
Elber <elbercastillomesa@gmail.com>
2020-01-17 00:00:38 -0500

    installed nodemon, first local for dev, but failed, then global

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 //Setup express app
 const app = express();
 
+app.get('/api', function(req, res){
+  console.log('GET request');
+  res.send({test:'message'});
+})
+
 //Liste for requests
 app.listen(process.env.port || 4000, function(){
   console.log('test app listening');
